f323066ad7d54692c59cb99b31d0ddd4762f6a93
Mustafa <91826370+developermstf01@users.noreply.github.com>
2022-09-14 20:31:19 +0300

    Çok boyutlu diziler ile B harfi

---
+++ BHarfiYaziyoruz.java
@@ -0,0 +1,29 @@
+public class BHarfiYaziyoruz {
+    public static void main(String[] args) {
+        String[][] letter = new String[5][4];
+
+        for (int i = 0; i < letter.length; i++){
+
+            for (int j = 0; j < letter[i].length; j++){
+
+                if ((i == 0) || (i == 2) || (i == 4)){
+                    letter[i][j] = " * ";
+                    letter[0][3] = "   ";
+                    letter[2][3] = "   ";
+                    letter[4][3] = "   ";
+
+                }else if ((j == 0) || (j == 3)){
+                    letter[i][j] = " * ";
+
+                }else letter[i][j] = "   ";
+            }
+        }
+
+        for (String[] row: letter){
+            for (String col: row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
